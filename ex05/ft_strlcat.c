5a110b1a3f8dcce15ec0f3b2831a3a0c48dc0af2
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-02-15 16:34:17 +0100

    Correção strlcat

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -6,26 +6,29 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 00:47:23 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/02/15 02:10:14 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/02/15 16:31:07 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int	i;
-	int	j;
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	r;
 	
 	j = 0;
 	i = 0;
-	while (dest[i] != '\0')
+	while (dest[i] != '\0' && i < size)
 		i++;
-	dest[i]++;
-	while (src[j] != '\0' && i < (int) size)
+	r = i;
+	while (src[j] != '\0' && i < size - 1)
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
 	}
+	while (src[j] != '\0')
+		j++;
 	dest[i] = '\0';
-	return ((unsigned int) i);
+	return (r + j);
 }
