bb84ab1cf0c528e4f46b01ee2cd28579c9b8714a
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-02-15 13:49:24 +0100

    Corrigido erro em ex04

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 23:56:57 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/02/15 00:22:48 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/02/15 13:48:50 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strstr(char *str, char *to_find)
 
 	i = 0;
 	j = 0;
+	if (to_find[j] == '\0')
+		return (str);
 	while (str[i] != '\0')
 	{
 		if (str[i] == to_find[0])
