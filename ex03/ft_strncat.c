129f286fabbf9ecbbd49a278b5a26f7bef2d000a
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-02-15 16:36:57 +0100

    Norminette

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -6,15 +6,15 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 20:47:18 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/02/15 15:01:04 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/02/15 16:36:00 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strncat(char *dest, char *src, unsigned int nb)
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
 	unsigned int	i;
 	unsigned int	j;
-	
+
 	j = 0;
 	i = 0;
 	while (dest[i] != '\0')
--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -6,16 +6,16 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 00:47:23 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/02/15 16:31:07 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2022/02/15 16:36:23 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
 	unsigned int	j;
 	unsigned int	r;
-	
+
 	j = 0;
 	i = 0;
 	while (dest[i] != '\0' && i < size)
