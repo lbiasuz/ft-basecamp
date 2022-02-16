7cad1fdbb304ac2292e52a7901ca3348690d36b7
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-02-16 03:03:42 +0100

    Exercício 04

---
+++ ex04/ft_putnbr_base.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 01:43:29 by lbiasuz           #+#    #+#             */
+/*   Updated: 2022/02/16 03:01:50 by lbiasuz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void ft_putnbr_base(int nbr, char *base)
+{
+	int		index;
+	int		sign;
+	int		bl;
+	char	arr[11];
+
+	index = 0;
+	sign = 1;
+	bl = ft_strlen(base);
+	if (nbr < 0)
+		sign = -1;
+	while (index < bl)
+	{
+		arr[index] = base[((nbr % bl) * sign)];
+		nbr = nbr / bl;
+		if (nbr == 0)
+			break ;
+		index++;
+	}
+	if (sign < 0)
+		write(1, "-", 1);
+	while (index >= 0)
+	{
+		write(1, &arr[index], 1);
+		index--;
+	}
+}
