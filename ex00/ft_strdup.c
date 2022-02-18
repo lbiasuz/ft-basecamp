0b5e578c0419db8881cd79b355a95d5259e2cc2a
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-02-19 00:58:04 +0100

    strdup

---
+++ ex00/ft_strdup.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/18 20:36:43 by lbiasuz           #+#    #+#             */
+/*   Updated: 2022/02/19 00:57:07 by lbiasuz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != 0)
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (*src)
+	{
+		dest[i] = *src;
+		src++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char *ft_strdup(char *src)
+{
+	char	*p;
+
+	p = malloc(ft_strlen(src));
+	if (p == 0)
+		return (0);
+	ft_strcpy(p, src);
+	return (p);
+}
