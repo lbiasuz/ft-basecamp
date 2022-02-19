d269f9db32ef780c657f592efcab48fd2306aed0
lbiasuz <biasuz.l.leonardo@gmail.com>
2022-02-19 12:34:38 +0100

    ft_ultimate_range

--- ex02/ft_ultimate_range.c
+++ ex02/ft_ultimate_range.c
@@ -1,33 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/19 00:59:36 by lbiasuz           #+#    #+#             */
-/*   Updated: 2022/02/19 02:34:14 by lbiasuz          ###   ########.fr       */
+/*   Created: 2022/02/19 12:05:08 by lbiasuz           #+#    #+#             */
+/*   Updated: 2022/02/19 12:30:35 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
 
-int	*ft_range(int min, int max)
+int	ft_ultimate_range(int **range, int min, int max)
 {
-	int	*p;
 	int	i;
 
 	if (min >= max)
 		return (0);
-	p = malloc((max - min) * 4);
-	if (p == 0)
+	*range = malloc((max - min) * 4);
+	if (*range == 0)
 		return (0);
 	i = 0;
 	while (min + i < max)
 	{
-		p[i] = min + i;
+		(*range)[i] = min + i;
 		i++;
 	}
-	return (p);
+	return (0);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int *p;
+	int **range;
+	int i;
+
+	range = &p;
+	ft_ultimate_range(range, 15, 70);
+	
+	for (i = 0; i < 55; i++)
+	{
+		printf("%d", (*range)[i]); 
+	}
 }
